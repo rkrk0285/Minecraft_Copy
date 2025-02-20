4e1289ec951f926d0c259b0c519997c76573629b
rkrk0285 <82379491+rkrk0285@users.noreply.github.com>
2025-02-20 23:30:32 +0900

    [TEST] First test commit
    - Create Project

---
+++ Source/Minecraft_Copy/Minecraft_Copy.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Minecraft_Copy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Minecraft_Copy, "Minecraft_Copy" );
---
+++ Source/Minecraft_Copy/Minecraft_CopyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Minecraft_CopyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINECRAFT_COPY_API AMinecraft_CopyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
